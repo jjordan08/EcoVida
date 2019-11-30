47835f6f41830207ed47958b75bb26ac5e6b6e4a
jjordan08 <jjordan_08@hotmail.com>
2019-11-30 00:15:02 -0500

    vistas completas

--- app/src/main/java/com/example/ecovida/escanearCodigo.java
+++ app/src/main/java/com/example/ecovida/escanearCodigo.java
@@ -3,12 +3,46 @@ package com.example.ecovida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class escanearCodigo extends AppCompatActivity {
+
+
+    EditText codigo;
+    Button scanner;
+    private ZXingScannerView vistaScanner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escanear_codigo);
+    }
+
+
+    public void buttonScanner (View view){
+
+        vistaScanner = new ZXingScannerView(this);
+        vistaScanner.setResultHandler(new zxingscanner());
+        setContentView(vistaScanner);
+        vistaScanner.startCamera();
+    }
+
+    class zxingscanner implements ZXingScannerView.ResultHandler{
+
+        @Override
+        public void handleResult(Result result) {
+            String dato = result.getText();
+            setContentView(R.layout.activity_escanear_codigo);
+            vistaScanner.stopCamera();
+            codigo = (EditText) findViewById(R.id.editText11);
+            codigo.setText(dato);
+        }
     }
 }
