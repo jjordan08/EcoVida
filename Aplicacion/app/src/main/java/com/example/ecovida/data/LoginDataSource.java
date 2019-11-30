a998ae70588cc486c797d901996e97ed46ff4d48
gabriel <gfvasquezn@gmail.com>
2019-11-30 13:02:48 -0500

    Organización Documentos

---
+++ Aplicacion/app/src/main/java/com/example/ecovida/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package com.example.ecovida.data;
+
+import com.example.ecovida.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
